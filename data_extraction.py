d7d561ecf6917ca9a91adbe6c68de1c5988cf3a4
praveencloudangles <112871831+praveencloudangles@users.noreply.github.com>
2024-10-04 11:31:50 +0530

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -3,12 +3,11 @@ import os
 
 
 def extract_data():
-
     s3 = boto3.client('s3')
     bucket_name = 'stock-market-usecase'
     url = s3.generate_presigned_url(
                     ClientMethod='get_object',
-                    Params={'Bucket': bucket_name, 'Key': 'Kidney_stone_detection.zip'},
+                    Params={'Bucket': bucket_name, 'Key': 'kidney_stone_reworked.zip'},
                     ExpiresIn=7200  # URL expiration time in seconds (adjust as needed)
                 )
     print(url)
